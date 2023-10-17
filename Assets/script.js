7c6d1f66547a342ad48c546f54b63dcf574459c8
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2023-10-17 15:09:46 -0700

    can print user's selections on webpage, current issue: modal will not close after hitting add project, had no issues closing it previsouly

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,50 @@
-
+// functions to display current time
 function currenDate() {
-    const currentTimeEl = document.querySelector('#time')
+    const currentTimeEl = $('#time')
     const date = dayjs().format('MMM D, YYYY [at] HH:mm:ss a')
+    currentTimeEl.text(date)
+}
+currenDate()
+setInterval(currenDate, 1000);
+
+// //variable
+
+const addProjectBtn = $('#add-project-btn');
+
+const projectNameInput = $('#project-name-input');
+
+const projectTypesInput = $('#project-types');
+
+const dueDateInput = $('#chosen-due-date');
+
+const tableHeadEl = $('table-head')
+const tableBodyEl = $('#table-body');
+
+function renderTable(e) {
+    e.preventDefault()
+
+    const trEl = $('<tr>') // each project will have one 
+
+    const tdName = $('<td>'); 
+    tdName.text(projectNameInput.val().trim());
+
+    const tdType = $('<td>');
+    tdType.text(projectTypesInput.val());
+
+    const tdDueDate = $('<td>');
+    tdDueDate.text(dueDateInput.val())
     
-    currentTimeEl.innerHTML = date
+    const tdButton = $('<td>');
+    const doneButton = $('<button>')
+    doneButton.text('X')
+
+    tableHeadEl.append(tableBodyEl);
+    tableBodyEl.append(trEl);
+    trEl.append(tdName);
+    trEl.append(tdType);
+    trEl.append(tdDueDate);
+    trEl.append(tdButton);
+    tdButton.append(doneButton);
 }
 
-setInterval(currenDate, 1000);
+addProjectBtn.on('click', renderTable);
